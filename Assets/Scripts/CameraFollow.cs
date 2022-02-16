c20ee7a1543ccd5e8c371e47b0953fde6802da52
IhsanSabri <ihsansabrisenturk@gmail.com>
2022-02-17 00:42:40 +0300

    Stack Fall Game

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 _camPosition;
+
+    [SerializeField] private Transform _player, _lastPlatform;
+    private float _cameraDistance = 5f;
+
+    void Awake()
+    {
+        _player = FindObjectOfType<Player>().transform;
+    }
+
+    void Update()
+    {
+        FollowTheBall();
+    }
+
+    private void FollowTheBall()
+    {
+        if (_lastPlatform == null)
+        {
+            _lastPlatform = GameObject.Find("LastPlatform(Clone)").transform;
+        }
+
+        _camPosition = new Vector3(transform.position.x, _player.transform.position.y, transform.position.z);
+
+        transform.position = new Vector3(transform.position.x, _camPosition.y, -_cameraDistance);
+    }
+}
